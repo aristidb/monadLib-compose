f9b22ca365282e4ae75b80d2a053227f428cab48
Aristid Breitkreuz <aristidb@googlemail.com>
2010-10-19 23:05:04 +0200

    derive_compose and instance for StateT

--- MonadLib/Compose.hs
+++ MonadLib/Compose.hs
@@ -31,13 +31,24 @@ instance Monad m => ComposeM (ReaderT s m) (ReaderT t m) s t where
       s <- n
       lift (runReaderT s m)
 
+derive_mcompose close open m n = do
+  s <- n
+  u <- lift $ mcompose (open m) (return s)
+  close u
+
 instance ComposeM m n s t => ComposeM (IdT m) (IdT n) s t where
-    mcompose m n = lift (mcompose (runIdT m) (runIdT n))
+    mcompose = derive_mcompose return runIdT
+
+instance (ComposeM m n s t, Monoid w) => ComposeM (WriterT w m) (WriterT w n) s t where
+    mcompose = derive_mcompose puts runWriterT
 
 instance ComposeM m n s t => ComposeM (ExceptionT e m) (ExceptionT e n) s t where
+    mcompose = derive_mcompose raises runExceptionT
+
+instance ComposeM m n s t => ComposeM (StateT i m) (StateT i n) s t where
     mcompose m n = do
       s <- n
-      u <- lift $ mcompose (runExceptionT m) (return s)
-      case u of
-        Left e -> raise e
-        Right x -> return x
+      i <- get
+      (u, i') <- lift $ mcompose (runStateT i m) (return s)
+      set i'
+      return u
